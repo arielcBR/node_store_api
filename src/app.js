da4c9c6a7c6d0dc19ba9c8c299e90e90e9c843ce
Ariel <ariel.campos94@yahoo.com>
2023-01-08 23:09:28 -0300

    Routes created

---
+++ src/app.js
@@ -0,0 +1,28 @@
+'use strict'
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const router = express.Router();
+
+// Conecta ao banco de dados
+mongoose.connect(process.env.DB_URL);
+
+// Carrega rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+// Configurando body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Middlewares
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+module.exports = app;
+
